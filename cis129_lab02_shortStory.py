52ada2d304b2187b554198ffb9e4e53d2971f3bc
esoto750 <esoto32@mail.pima.edu>
2025-02-07 18:04:57 -0700

    Update cis129_lab02_shortStory.py
    story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+#Tany Soto
+#first lab
+#short story
+
+print('everyday the woodchucker would chcuk wood')
+print('people would guess how much wood the would the woodchuck chucks')
+print('but the woodchuck cant chuck wood')
